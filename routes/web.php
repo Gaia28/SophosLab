ee0dff6c6adad77450bedf5e8356d0c4fdb659f4
Gaia28 <matheusgaia33@gmail.com>
2026-01-15 08:45:48 -0300

    removendo a página de welcome padrão | adicionando validação de autenticação nas rotas

--- routes/web.php
+++ routes/web.php
@@ -8,18 +8,18 @@ use App\Livewire\Produtos\CriarProduto;
 use App\Livewire\Produtos\ListarProdutos;
 use App\Livewire\Vendas\RegistrarVenda;
 use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
 
-Route::view('/', 'welcome');
-
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+ Volt::route('/', 'pages.auth.login')
+        ->name('login');
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-// Rotas de Materiais
+Route::middleware(['auth', 'verified'])->group(function () {
+Route::get('/dashboard', App\Livewire\Dashboard::class)->name('dashboard');
+
 Route::get('/materiais', MaterialIndex::class)->name('materiais.index');
 Route::get('/materiais/criar', MaterialCreate::class)->name('materiais.create');
 Route::get('/materiais/{material}/editar', MaterialEdit::class)->name('materiais.edit');
@@ -29,4 +29,5 @@ Route::get('/produtos', ListarProdutos::class)->name('produtos.index');
 
 Route::get('/vendas/nova', RegistrarVenda::class)->name('vendas.create');
 
+});
 require __DIR__.'/auth.php';
